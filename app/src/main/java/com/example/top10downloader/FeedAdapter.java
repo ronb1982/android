ade307c8babd585fce8c0e503a0d2908ed2bca11
ronb <ronbnj@gmail.com>
2017-10-14 18:40:21 -0400

    Added menu items and supporting methods

---
+++ app/src/main/java/com/example/top10downloader/FeedAdapter.java
@@ -0,0 +1,72 @@
+package com.example.top10downloader;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class FeedAdapter extends ArrayAdapter {
+    private static final String TAG = "FeedAdapter";
+    private final int layoutResource;
+    private final LayoutInflater layoutInflater;
+    private List<FeedEntry> applications;
+
+    public FeedAdapter(Context context, int resource, List<FeedEntry> applications) {
+        super(context, resource);
+        this.layoutResource = resource;
+        this.layoutInflater = LayoutInflater.from(context);
+        this.applications = applications;
+    }
+
+    @Override
+    public int getCount() {
+        return applications.size();
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        ViewHolder viewHolder;
+
+        // Create view with the LayoutInflater by using the layoutResource
+        if (convertView == null) {
+            Log.d(TAG, "getView: called with null convertView");
+            convertView = layoutInflater.inflate(layoutResource, parent, false);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        } else {
+            Log.d(TAG, "getView: provided a convertView");
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        // Set name, artist, and summary on FeedEntry view
+        FeedEntry currentApp = applications.get(position);
+
+        // Set fields
+        viewHolder.tvName.setText(currentApp.getName());
+        viewHolder.tvArtist.setText(currentApp.getArtist());
+        viewHolder.tvSummary.setText(currentApp.getSummary());
+
+        return convertView;
+    }
+
+    // Nested class that holds the state of each View
+    private class ViewHolder {
+        final TextView tvName;
+        final TextView tvArtist;
+        final TextView tvSummary;
+
+        ViewHolder(View v) {
+            this.tvName = (TextView) v.findViewById(R.id.tvName);
+            this.tvArtist = (TextView) v.findViewById(R.id.tvArtist);
+            this.tvSummary = (TextView) v.findViewById(R.id.tvSummary);
+        }
+    }
+}
