21e08c1a5144f279c9d46e118a3ae184d2bbd28c
ronb <ronbnj@gmail.com>
2017-10-21 18:54:53 -0400

    Initial commit

---
+++ app/src/main/java/com/coderversity/flickrbrowser/GetRawData.java
@@ -0,0 +1,104 @@
+package com.coderversity.flickrbrowser;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by ronb on 10/21/17.
+ */
+
+enum DownloadStatus { IDLE, PROCESSING, NOT_INITIALISED, FAILED_OR_EMPTY, OK }
+
+class GetRawData extends AsyncTask<String, Void, String> {
+    private static final String TAG = "GetRawData";
+
+    private DownloadStatus mDownloadStatus;
+    private final OnDownloadComplete mCallback;
+
+    // This interface forces implementation of the onDownloadComplete() method in the caller class where this interface is implemented.
+    interface OnDownloadComplete {
+        void onDownloadComplete(String data, DownloadStatus status);
+    }
+
+    public GetRawData(OnDownloadComplete callback) {
+        mDownloadStatus = DownloadStatus.IDLE;
+        mCallback = callback;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        // param "s" is a JSON response object
+        Log.d(TAG, "onPostExecute: parameter equals " + s);
+
+        if (mCallback != null) {
+            mCallback.onDownloadComplete(s, mDownloadStatus);
+        }
+        Log.d(TAG, "onPostExecute: ends");
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        HttpURLConnection connection = null;
+        BufferedReader reader = null;
+
+        if (strings == null) {
+            mDownloadStatus = DownloadStatus.NOT_INITIALISED;
+            return null;
+        }
+
+        // Call REST API
+        try {
+            mDownloadStatus = DownloadStatus.PROCESSING;
+            URL url = new URL(strings[0]);
+
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            int response = connection.getResponseCode();
+            Log.d(TAG, "doInBackground: The response code was " + response);
+
+            StringBuilder result = new StringBuilder();
+            reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+//            String line;
+//            while ((line = reader.readLine()) != null) {
+//                result.append(line).append("\n");
+//            }
+
+            for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+                result.append(line).append("\n");
+            }
+
+            mDownloadStatus = DownloadStatus.OK;
+            return result.toString();
+
+        } catch(MalformedURLException e) {
+            Log.e(TAG, "doInBackground: Invalid URL " + e.getMessage());
+        } catch(IOException e) {
+            Log.e(TAG, "doInBackground: IO Esception reading data: " + e.getMessage());
+        } catch(SecurityException e) {
+            Log.e(TAG, "doInBackground: Security Exception. Needs permission? " + e.getMessage());
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    Log.e(TAG, "doInBackground: Error closing stream " + e.getMessage());
+                }
+            }
+        }
+
+        mDownloadStatus = DownloadStatus.FAILED_OR_EMPTY;
+        return null;
+    }
+}
---
+++ app/src/main/java/com/coderversity/flickrbrowser/MainActivity.java
@@ -0,0 +1,63 @@
+package com.coderversity.flickrbrowser;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity implements GetRawData.OnDownloadComplete {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: starts");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        GetRawData getRawData = new GetRawData(this);
+        getRawData.execute("https://api.flickr.com/services/feeds/photos_public.gne?tags=android,nougat,sdk&tagmode=any&format=json&nojsoncallback=1");
+
+        Log.d(TAG, "onCreate: ends");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        Log.d(TAG, "onCreateOptionsMenu() returned: " + true);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        Log.d(TAG, "onOptionsItemSelected() returned: returned");
+        return super.onOptionsItemSelected(item);
+    }
+
+    // Callback method that gets called when something happens in the called object (e.g. GetRawData).
+    // The called object (class) will then get a reference to the caller object containing this method.
+    @Override
+    public void onDownloadComplete(String data, DownloadStatus status) {
+        if (status == DownloadStatus.OK) {
+            Log.d(TAG, "onDownloadComplete: data is " + data);
+        } else {
+            // download or processing failed
+            Log.e(TAG, "onDownloadComplete: failed with status " + status);
+        }
+    }
+}
